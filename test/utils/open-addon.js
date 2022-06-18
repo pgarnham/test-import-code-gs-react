168e9e7346cd889f13e110d40dbdd4460b71aacd
Elisha Nuchi <31550519+enuchi@users.noreply.github.com>
2022-06-18 14:15:09 -0400

    refactor google verification flow

--- test/utils/open-addon.js
+++ test/utils/open-addon.js
@@ -17,47 +17,71 @@ const openAddon = async (page) => {
   await page.waitForTimeout(3000);
 
   if (
+    // some kind of verification screen appears
     await page.evaluate(
       () =>
         document.querySelector('h1#headingText') &&
-        document.querySelector('h1#headingText').innerText.includes('erify') &&
-        document.querySelector('body').innerText.includes('verification code')
+        document.querySelector('h1#headingText').innerText.includes('erify')
     )
   ) {
-    const fullHtml = await page.evaluate(() => document.body.outerHTML);
-    console.log(fullHtml);
-    await page.screenshot({
-      path: './test/__image_snapshots__/verification_screenshot.png',
-    });
-
-    await page.waitForTimeout(20000);
-
-    let response;
-    axios.get(process.env.VERIFICATION_SHEET_URL).then((res) => {
-      response = res.data;
-    });
-
-    const verificationCode = response.values[0][0];
-
-    console.log('verificationCode', verificationCode);
-
-    await page.type('input[name="idvPin"]', verificationCode); // type verification code
-    await page.waitForTimeout(6000);
-    await page.click('button'); // click "next" button
-    await page.waitForTimeout(5000);
-
+    // first print html and save pic
+    console.log('on some verification page');
     console.log(await page.evaluate(() => document.body.outerHTML));
     await page.screenshot({
-      path: './test/__image_snapshots__/verification_screenshot_next_page.png',
+      path: './test/__image_snapshots__/verification_screenshot_1.png',
     });
-  } else if (
-    await page.evaluate(
-      () =>
-        document.querySelector('h1#headingText') &&
-        document.querySelector('h1#headingText').innerText.includes('erify') &&
-        !document.querySelector('body').innerText.includes('verification code')
-    )
-  ) {
+    await page.waitForTimeout(1000);
+
+    // check if on verification page, too many tries, need to choose "use recovery email" method:
+    if (
+      await page.evaluate(() =>
+        document
+          .querySelector('body')
+          .innerText.includes('confirm your recovery email')
+      )
+    ) {
+      // first print html and save pic
+      console.log('page contains recovery email');
+
+      await page
+        .evaluate(
+          "//div[contains(., 'Confirm your recovery email')]",
+          document,
+          null,
+          XPathResult.ANY_TYPE,
+          null
+        )
+        .iterateNext()
+        .click();
+      await page.waitForTimeout(3000);
+    } else if (
+      // already on verification code page
+      await page.evaluate(() => document.querySelector('input[name="idvPin"]'))
+    ) {
+      // first print html and save pic
+      console.log('page has idvPin input');
+
+      let response;
+      axios.get(process.env.VERIFICATION_SHEET_URL).then((res) => {
+        response = res.data;
+      });
+      const verificationCode = response.values[0][0];
+
+      console.log('verificationCode', verificationCode);
+      await page.type('input[name="idvPin"]', verificationCode); // type verification code
+      await page.waitForTimeout(6000);
+      await page.click('button'); // click "next" button
+      await page.waitForTimeout(5000);
+
+      // first print html and save pic
+      console.log('typed in idvPin, clicked button, on new page');
+      console.log(await page.evaluate(() => document.body.outerHTML));
+      await page.screenshot({
+        path: './test/__image_snapshots__/verification_screenshot_2.png',
+      });
+      await page.waitForTimeout(1000);
+    }
+
     try {
       await page.click('div[data-accountrecovery]');
       await page.waitForTimeout(6000);
@@ -66,11 +90,13 @@ const openAddon = async (page) => {
       console.log('The "choose account recovery method" page isn\'t shown');
     }
 
-    const fullHtml = await page.evaluate(() => document.body.outerHTML);
-    console.log(fullHtml);
+    // first print html and save pic
+    console.log('clicked go to recovery email page');
+    console.log(await page.evaluate(() => document.body.outerHTML));
     await page.screenshot({
-      path: './test/__image_snapshots__/windows_screenshot.png',
+      path: './test/__image_snapshots__/verification_screenshot_3.png',
     });
+    await page.waitForTimeout(1000);
 
     await page.type(
       'input[name="knowledgePreregisteredEmailResponse"]',
